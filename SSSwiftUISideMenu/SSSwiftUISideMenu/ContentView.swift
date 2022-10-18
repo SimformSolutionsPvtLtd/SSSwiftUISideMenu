fda39eb7141259a87dac6af9ba27fd8f480d99b8
Parth Dumaswala <parth.du@simformsolutions.com>
2022-10-18 12:11:51 +0530

    Initial commit

---
+++ SSSwiftUISideMenu/SSSwiftUISideMenu/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SSSwiftUISideMenu
+//
+//  Created by Parth Dumaswala on 18/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
