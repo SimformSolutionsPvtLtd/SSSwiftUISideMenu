eeff25891adffa1db108be7cbc9989eca788d20a
Parth Dumaswala <parth.du@simformsolutions.com>
2023-10-31 18:30:50 +0530

    UNT-T19852: Update ReadMe, package.swift and podspecs

--- Package.swift
+++ Package.swift
@@ -12,10 +12,19 @@ let package = Package(
             name: "SSSwiftUISideMenu",
             targets: ["SSSwiftUISideMenu"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        // Targets are the basic building blocks of a package.
+        // A target can define a module or a test suite.
+        // Targets can depend on other targets in this package,
+        // and products in packages this package depends on.
         .target(
-            name: "SSSwiftUISideMenu"),
+            name: "SSSwiftUISideMenu",
+            dependencies: [],
+            path: "Sources"
+        )
     ]
 )
--- Sources/SSSwiftUISideMenu/SSSwiftUISideMenu.swift
+++ Sources/SSSwiftUISideMenu/SSSwiftUISideMenu.swift
@@ -11,8 +11,15 @@ public struct MenuItem : Hashable {
     
     var title : String
     var icon : String
+    
+    public init(title: String, icon: String) {
+        self.title = title
+        self.icon = icon
+    }
+    
 }
 
+@available(iOS 16.0, *)
 public struct SSSwiftUISideMenu: View {
     
     @Binding var openSideMenu: Bool
@@ -32,13 +39,14 @@ public struct SSSwiftUISideMenu: View {
         self.menuConfig = menuConfig
     }
     
+    private var viewWidth = UIScreen.main.bounds.width
     public var body: some View {
         
         ZStack {
             GeometryReader { _ in
                 EmptyView()
             }
-            .background(Color.gray.opacity(0.3))
+            .background(Color.black.opacity(0.2))
             .opacity(self.openSideMenu ? 1.0 : 0.0)
             .animation(menuConfig.animationType.delay(0.25), value: 0)
             
@@ -46,43 +54,73 @@ public struct SSSwiftUISideMenu: View {
                 
                 VStack {
                     VStack(alignment: .leading, spacing: menuConfig.rowPadding) {
-                        
-                        ForEach(0..<menuItems.count, id: \.self) { value in
-                            
-                            Button(action: {
-                                withAnimation(.spring()) {
-                                    self.selectedIndex = value
-                                    self.openSideMenu.toggle()
-                                }
-                            }, label: {
-                                HStack(spacing: menuConfig.imageToTitlePadding) {
-                                    Image(menuItems[value].icon)
-                                        .renderingMode(.template)
-                                        .foregroundColor(menuConfig.iconTintColor)
-                                        .frame(width: menuConfig.iconWidth, height: menuConfig.iconHeight)
-                                    Text(menuItems[value].title)
-                                        .font(menuConfig.titleFont)
-                                        .fontWeight(.semibold)
-                                }
-                                .foregroundColor(selectedIndex == value ? menuConfig.selectedTitleColor : menuConfig.titleColor)
-                                .padding(.vertical, menuConfig.rowPadding)
-                                .padding(.horizontal, menuConfig.leftIconPadding)
-                                .frame(maxWidth: UIScreen.main.bounds.width - 170, alignment: .leading)
-                            })
-                            
+                        List {
+                            ForEach(0..<menuItems.count, id: \.self) { value in
+                                
+                                Button(action: {
+                                    withAnimation(.spring()) {
+                                        self.selectedIndex = value
+                                        self.openSideMenu.toggle()
+                                    }
+                                }, label: {
+                                    HStack(spacing: menuConfig.imageToTitlePadding) {
+                                        Image(menuItems[value].icon)
+                                            .renderingMode(.template)
+                                            .foregroundColor(menuConfig.iconTintColor)
+                                            .frame(width: menuConfig.iconWidth, height: menuConfig.iconHeight)
+                                        Text(menuItems[value].title)
+                                            .font(menuConfig.titleFont)
+                                            .fontWeight(.semibold)
+                                    }
+                                    .foregroundColor(selectedIndex == value ? menuConfig.selectedTitleColor : menuConfig.titleColor)
+                                    .padding(.vertical, menuConfig.rowPadding)
+                                    .padding(.horizontal, menuConfig.leftIconPadding)
+                                    .frame(maxWidth: UIScreen.main.bounds.width - 170, alignment: .leading)
+                                })
+                            }
+                            .listRowSeparator(.hidden)
+                            .listRowInsets(.init())
+                            .buttonStyle(.plain)
+                            .listRowBackground(Color.clear)
                         }
+                        .scrollIndicators(.hidden)
+                        .listStyle(.plain)
                     }
                     .padding(.top, menuConfig.topPadding)
+                    
+                    if menuConfig.showAppVersion {
+                        VStack() {
+                            Text(menuConfig.versionText)
+                                .padding()
+                                .frame(maxWidth: .infinity, alignment: menuConfig.versionAlignment)
+                                .foregroundStyle(menuConfig.versionTitleColor)
+                                .font(menuConfig.versionFont)
+                        }
+                    }
                 }
                 .padding()
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-                
                 .frame(width: menuConfig.menuWidth)
                 .background(menuConfig.backgroundColor)
-                .offset(x: self.openSideMenu ? 0 : -menuConfig.menuWidth)
+                .offset(x: self.openSideMenu ? menuConfig.menuDirection == .left ? 0 : viewWidth - menuConfig.menuWidth : menuConfig.menuDirection == .left ? -menuConfig.menuWidth : viewWidth)
                 .animation(menuConfig.animationType, value: openSideMenu)
                 Spacer()
             }
-        }
+            
+        }.gesture(DragGesture(minimumDistance: 20, coordinateSpace: .global)
+            .onEnded { value in
+                if !menuConfig.swipeToClose { return }
+                let horizontalAmount = value.translation.width
+                let verticalAmount = value.translation.height
+                
+                if abs(horizontalAmount) > abs(verticalAmount) {
+                    if menuConfig.menuDirection == .left && horizontalAmount < 0 {
+                        self.openSideMenu.toggle()
+                    } else if menuConfig.menuDirection == .right && horizontalAmount > 0 {
+                        self.openSideMenu.toggle()
+                    }
+                }
+            })
     }
+    
 }
